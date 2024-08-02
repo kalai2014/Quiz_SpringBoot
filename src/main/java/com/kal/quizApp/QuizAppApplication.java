31ca6656ee79c5e6e94de0a26304aec8383f91ae
Kal <kalainidhi1999@gmail.com>
2024-08-02 18:44:25 -0500

    this is the last commit

--- src/main/java/com/kal/quizApp/QuizAppApplication.java
+++ src/main/java/com/kal/quizApp/QuizAppApplication.java
@@ -9,6 +9,7 @@ public class QuizAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(QuizAppApplication.class, args);
 		System.out.println("secojfds");
+		System.out.println("sdfdasasdf");
 	}
 
 }
