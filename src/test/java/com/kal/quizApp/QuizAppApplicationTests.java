fdf4b31f4143d9bac8533fcd4c38c1ca52c892d5
Kal <kalainidhi1999@gmail.com>
2024-08-02 18:21:51 -0500

    First commit

---
+++ src/test/java/com/kal/quizApp/QuizAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kal.quizApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
